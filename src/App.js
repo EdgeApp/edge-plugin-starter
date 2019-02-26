a46ec7fa96388641fbfe5691209445a08d7e77cd
ahujsak <allen@missionvi.com>
2019-02-26 13:34:19 -0500

    update readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, { Component } from 'react';
+
 class App extends Component {
+  getCurrencyWallet = () => {
+    window.edgeProvider.chooseCurrencyWallet(['BTC', 'ETH'])
+    .then(result => {
+      console.log('I have a chooseCurrencyWallet result', result)
+    })
+  }
+  getCurrencyWalletAllCurrencies = () => {
+    window.edgeProvider.chooseCurrencyWallet()
+    .then(result => {
+      console.log('I have a chooseCurrencyWallet ALL result', result)
+    })
+  }
+  getReceiveAddress = () => {
+    const edgeMetadata = {
+      name: 'Test Drive ',
+      category: 'Plugin Stuff ',
+      notes: 'No Notes really',
+    }
+    const options = {
+      metadata: edgeMetadata
+    }
+    window.edgeProvider.getReceiveAddress(options)
+    .then(result => {
+      console.log('I have a getReceiveAddress result', result)
+    })
+  }
+  writeData = () => {
+    const data = {
+      myProp: 'Here is what I am thinking'
+    }
+    window.edgeProvider.writeData(data)
+    .then(result => {
+      console.log('I have a writeData result', result)
+    })
+  }
+  readData = () => {
+    const keys = ['myProp', 'yourProp']
+    window.edgeProvider.readData(keys)
+    .then(result => {
+      console.log('I have a readData result', result)
+    })
+  }
+  requestSpend = () => {
+    const spendTargets = [
+      {
+        nativeAmount: '0.000877',
+        publicAddress: '1DfgUAznhjjRNiFCRxTD2Tdzr9psdZ1oAD'
+      }
+    ]
+    window.edgeProvider.requestSpend(spendTargets)
+    .then(result => {
+      console.log('I have a requestSpend result', result)
+    })
+  }
+  requestSpendWithOptions = () => {
+    const spendTargets = [
+      {
+        nativeAmount: '0.000877',
+        publicAddress: '1DfgUAznhjjRNiFCRxTD2Tdzr9psdZ1oAD'
+      }
+    ]
+    const options = {
+      
+    }
+    window.edgeProvider.requestSpend(spendTargets, options)
+    .then(result => {
+      console.log('I have a requestSpend result', result)
+    })
+  }
+  requestSpendUri = () => {
+    const uri = 'bitcoin:1DfgUAznhjjRNiFCRxTD2Tdzr9psdZ1oAD?amount=0.000877'
+    window.edgeProvider.requestSpendUri(uri)
+    .then(result => {
+      console.log('I have a requestSpendUri result', result)
+    })
+  }
+  requestSpendUriWithOptions = () => {
+    const uri = 'bitcoin:1DfgUAznhjjRNiFCRxTD2Tdzr9psdZ1oAD?amount=0.000877'
+    const options = { //EdgeRequestSpendOptions
+
+    }
+    window.edgeProvider.requestSpendUri(uri, options)
+    .then(result => {
+      console.log('I have a requestSpendUri result', result)
+    })
+  }
+  signMessage = () => {
+    const options = { //EdgeSignMessageOptions
+
+    }
+    window.edgeProvider.signMessage(options)
+    .then(result => {
+      console.log('I have a signMessage result', result)
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button type="init" className="btn btn-success" onClick={this.getCurrencyWallet}>Get Limited Wallets</button>
+          <div className="App-shim" />
+          <button type="startTimer" className="btn btn-success" onClick={this.getCurrencyWalletAllCurrencies}>Get All Wallets</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.getReceiveAddress}>Get Receive Address</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.writeData}>Write Data</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.readData}>Read Data</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.requestSpend}>Request Spend</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.requestSpendWithOptions}>Request Spend With Options</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.requestSpendUri}>Request Spend URI</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.requestSpendUriWithOptions}>Request Spend URI w/Options</button>
+          <div className="App-shim" />
+          <button type="next" className="btn btn-success" onClick={this.signMessage}>Sign Message</button>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// look
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
